f4363fb1464d4e4e7b9abca863f9065f4f506ee9
Juggemmy <emmaadetoye24@gmail.com>
2023-06-01 15:20:20 +0100

    first commit

---
+++ library_management/library_management/doctype/library_membership/test_library_membership.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Emerald Uwalaka-Henry and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryMembership(FrappeTestCase):
+	pass
